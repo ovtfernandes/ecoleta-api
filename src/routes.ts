a77006a24c6f776ec99a799ea43d59301251faf3
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-06-02 13:24:24 -0300

    Moves create point function to points controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/points-controller';
+
 const routes = express.Router();
+const pointsController = new PointsController();
 
 routes.get('/items', async (req, res) => {
     const items = await knex('items').select('*');
@@ -18,41 +21,6 @@ routes.get('/items', async (req, res) => {
     return res.json(serializedItems);
 });
 
-routes.post('/points', async (req, res) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items,
-    } = req.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => ({
-        item_id,
-        point_id,
-    }));
-
-    await trx('point_items').insert(pointItems);
-
-    return res.json({ success: true });
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
