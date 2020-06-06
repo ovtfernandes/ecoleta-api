049bfd7259bd83552e8c781dfb60986606596a2c
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-06-06 14:37:42 -0300

    Adds upload functionality

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointsController from './controllers/points-controller';
 import ItemsController from './controllers/items-controller';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
+
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
